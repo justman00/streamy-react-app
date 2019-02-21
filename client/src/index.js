7f442b0b392740083099735f139f864df3173441
Vlad <vovaturcan4@gmail.com>
2019-02-22 01:54:46 +0200

    hooked up redux and sored the user id and the signedIn value

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);

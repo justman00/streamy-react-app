a6c82d4733c9065ad2060bde3065baf7bfd78bf9
Vlad <vovaturcan4@gmail.com>
2019-02-23 16:16:33 +0200

    hooked up the stream reducer and made it work

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import reducers from "./reducers";
-
+import reduxThunk from "redux-thunk";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
